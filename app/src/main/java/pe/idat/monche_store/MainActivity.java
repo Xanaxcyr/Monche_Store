4fa55a22b3bfcc23bfbb4585345f6ad2bdfa9169
Xanaxcyr <becanroman365@gmail.com>
2023-12-17 09:40:36 -0500

    Start commit

---
+++ app/src/main/java/pe/idat/monche_store/MainActivity.java
@@ -0,0 +1,15 @@
+
+package pe.idat.monche_store;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
